8db6c04aa85d637412e92225280f5ee82ebf3843
Stetson Ramey <stetson.ramey@gmail.com>
2018-04-04 18:18:33 -0500

    initial commit, general file structure

---
+++ letter.js
@@ -0,0 +1,17 @@
+var Letter = function (character, placeholder) {
+    this.character = "";
+    this.placeholder = "_";
+    this.letterGuessed = false;
+
+    this.guess = function () {
+        
+    }
+
+    this.guessCheck = function () {
+        
+    }
+}
+
+// module.exports = Letter; 
+
+// test locally then uncomment the exports line above
---
+++ word.js
@@ -0,0 +1,17 @@
+var Letter = require("./letter");
+
+var Word = function (wordToGuess) {
+    this.new = [];
+
+    // function 1 here
+
+    // need a function here that returns the string representing the word.  This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+
+    // function 2 here
+
+    // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+
+}
+
+
+// module.exports = Word; 
